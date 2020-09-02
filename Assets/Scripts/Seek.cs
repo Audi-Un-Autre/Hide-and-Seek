890856eed741c91bd8501859be61dfce7198c679
Audrey Williams <43081195+Audi-Un-Autre@users.noreply.github.com>
2020-09-01 22:24:47 -0400

    Testing rotation algorithm

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -8,26 +8,42 @@ public class Seek : MonoBehaviour
     public GameObject floor;
     public float moveSpeed = 2f, rotSpeed = 3f;
     [SerializeField] Vector3 destination;
-    [SerializeField] bool destinationSet = false, checking = false;
-    [SerializeField] float time;
+    [SerializeField] bool destinationSet = false, checking = false, gotAngle = false;
+    [SerializeField] float timer;
+    [SerializeField] float randomAngle = 0f;
 
     void Start(){
     }
 
     void Update(){
         float maxCheck = 5.0f;
-
+        
+        // Set a random destination
         if (!destinationSet){
-            time = 0.0f;
+            timer = 0.0f;
             destination = SetDestination(floor);
             destinationSet = true;
-        } else if (destinationSet){
+        }
+        
+        if (destinationSet && !checking){
             MoveToDestination(destination);
-            if (CheckPosition(destination)){
-                CheckAround();
-                time += Time.deltaTime;
-                if (time >= maxCheck)
-                    destinationSet = false;
+        }
+        
+        if (destinationSet && CheckPosition(destination)){
+            checking = true;
+            if (!gotAngle){
+                randomAngle = Random.Range(-180f, 180f);
+                gotAngle = true;
+            }
+            else{
+                Quaternion desiredRotation = Quaternion.Euler(0.0f, randomAngle, 0.0f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, 5f * Time.deltaTime);
+            }
+            timer += Time.deltaTime;
+            if (timer >= maxCheck){
+                checking = false;
+                gotAngle = false;
+                destinationSet = false;
             }
         }
     }
@@ -46,7 +62,7 @@ public class Seek : MonoBehaviour
     private void MoveToDestination(Vector3 point){
         // Face and move towards point
         Quaternion rotation = Quaternion.LookRotation(point - transform.position);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(point - transform.position), rotSpeed * Time.deltaTime);
         transform.position = Vector3.MoveTowards(transform.position, point, moveSpeed * Time.deltaTime);
     }
 
@@ -59,18 +75,8 @@ public class Seek : MonoBehaviour
         }
     }
 
-    public void CheckAround(){
+    //public void CheckAround(){
         // Seeker will look around at a random spot over 5 seconds
-        float tempRotSpeed = 2f;
-        float timer = 0f;
-        float maxTimer = 4.5f;
-        Quaternion endRot = Quaternion.Euler(new Vector3(0.0f, Random.Range(-180f, 180f), 0.0f));
-        //Vector3 newRot = new Vector3(0.0f, Random.Range(-180.0f, 180.0f), 0.0f);
-
-        //timer += Time.deltaTime;
-        //if (timer > maxTimer)
-            //endRot = Quaternion.Euler(newRot);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, endRot, tempRotSpeed * Time.deltaTime);
-    }
+        
+    //}
 }
